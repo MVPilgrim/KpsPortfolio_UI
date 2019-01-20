c4379634b3155d6674aa293f1952e99db725365f
MVPilgrim <mv.pilgrim.aws@gmail.com>
2019-01-20 22:33:20 +0000

    Adding NavMenu.

--- ui/DoistHelloWorld/src/NavMenu.js
+++ ui/DoistHelloWorld/src/NavMenu.js
@@ -4,12 +4,18 @@ export default class NavMenu extends Component {
   render() {
     return(
       <div class="NavMenu">
-        <p/>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="about.html">About</a>
-        <p/>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="gospel.html">The Gospel</a>
-        <p/>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="doistMain.html">Doist Demo</a>
-        <p/>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="virtualSupport.html">Virtual Support</a>
-        <p/>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="wiseEliza.html">Wise Eliza</a>
-        <p/>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="childWebPage.html">Child-like Web Page</a>
+        <div>&nbsp;</div>
+        <div>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="about.html">About</a></div>
+        <div>&nbsp;</div>
+        <div>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="gospel.html">The Gospel</a></div>
+        <div>&nbsp;</div>
+        <div>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="doistMain.html">Doist Demo</a></div>
+        <div>&nbsp;</div>
+        <div>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="virtualSupport.html">Virtual Support</a></div>
+        <div>&nbsp;</div>
+        <div>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="wiseEliza.html">Wise Eliza</a></div>
+        <div>&nbsp;</div>
+        <div>&nbsp;<i class="far fa-folder">&nbsp;&nbsp;</i><a href="childlikeHomepage.html">Childlike Homepage</a></div>
      </div>
     );
   }
