4969d4f5736791efb9faf50da9b8ee873a410ebc
MVPilgrim <mv.pilgrim.aws@gmail.com>
2023-04-23 17:59:00 -0500

    Prep for Jenkins.

--- buildui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clear
-
-npm run-script build
-
-sudo rm -f /usr/share/nginx/html/app* /usr/share/nginx/html/index.html /usr/share/nginx/html/runtime* /usr/share/nginx/html/vend*
-sudo cp -r dist/* /usr/share/nginx/html
