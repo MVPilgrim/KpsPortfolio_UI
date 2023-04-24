5368442d29bff224b4665adbe47ed601854f52de
MVPilgrim <mv.pilgrim.aws@gmail.com>
2023-04-23 22:59:42 -0500

    get buildWebsite.sh to work

--- buildWebsite.sh
+++ buildWebsite.sh
@@ -4,10 +4,12 @@ nginxPath=/usr/share/nginx/html
 #nginxPath=/usr/share/nginx/html/html2
 sudo mkdir -p $nginxPath
 
-kpWebsite=/home/ec2-user/KpsPortfolio/KpsPortfolio_Website/src
+kpsWebsite=/home/ec2-user/KpsPortfolio/KpsPortfolio_Website
 
 clear
 
+cd $kpsWebsite
+pwd
 npm run-script build
 
 #sudo rm -f $nginxPath/app* $nginxPath/index.html $nginxPath/runtime* $nginxPath/html/vend*
